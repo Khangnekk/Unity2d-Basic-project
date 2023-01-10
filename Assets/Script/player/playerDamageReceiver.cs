3f96d4709b738c4f70e566ec3bfa3063270cbc35
Khangnekk <contact.khalgfk@gmail.com>
2023-01-10 13:51:14 +0700

    Square War V2.2.6

--- Assets/Script/player/playerDamageReceiver.cs
+++ Assets/Script/player/playerDamageReceiver.cs
@@ -1,10 +1,8 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class playerDamageReceiver : damageReceiver
 {
     protected playerController playerController;
-    public Text hpTxt;
     public AudioSource oughSound;
     private void Start()
     {
@@ -13,13 +11,7 @@ public class playerDamageReceiver : damageReceiver
     public override void Receive(int damage)
     {
         base.Receive(damage);
-        getHP();
         oughSound.Play();
         if (isDead()) playerController.playerStatus.Dead();
-    }
-
-    protected void getHP()
-    {
-        hpTxt.text = "HP: " + hp.ToString();
     }
 }
